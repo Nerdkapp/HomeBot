e68595551cb2a5d9d85dcdabf4635cb3e9de0f20
Luciano Coccia <luciano.coccia@lastminute.com>
2016-07-03 20:46:15 +0200

    Now the camera endpoint is configurable

--- src/main/java/com/nerdkapp/homebot/infrastructure/iot/RestRaspberryCamera.java
+++ src/main/java/com/nerdkapp/homebot/infrastructure/iot/RestRaspberryCamera.java
@@ -4,6 +4,7 @@ import com.nerdkapp.homebot.domain.iot.Camera;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -21,10 +22,17 @@ public class RestRaspberryCamera implements Camera
   private final Logger log = LoggerFactory.getLogger(this.getClass());
   private RestTemplate restTemplate = new RestTemplate();
 
+  private String cameraEndpoint;
+
+  public RestRaspberryCamera(@Value("${camera.endpoint:http://192.168.1.101:8080}") String cameraEndpoint)
+  {
+    this.cameraEndpoint = cameraEndpoint;
+  }
+
   @Override
   public ByteArrayResource getPicture()
   {
-    PictureLocation pictureLocation = restTemplate.getForEntity("http://192.168.1.101:8080/takePicture", PictureLocation.class).getBody();
+    PictureLocation pictureLocation = restTemplate.getForEntity(cameraEndpoint + "/takePicture", PictureLocation.class).getBody();
 
     log.info("Image retrieved from camera: {}", pictureLocation);
     final String filename = "somefile.jpg";
@@ -32,7 +40,7 @@ public class RestRaspberryCamera implements Camera
     ByteArrayResource contentsAsResource = null;
     try
     {
-      try(InputStream in = new URL("http://192.168.1.101:8080" + pictureLocation.getImage()).openStream()){
+      try(InputStream in = new URL(cameraEndpoint + pictureLocation.getImage()).openStream()){
         Files.copy(in, Paths.get("/tmp/" + UUID.randomUUID()));
       }
 
--- src/test/java/com/nerdkapp/homebot/infrastructure/iot/RestRaspberryCameraIT.java
+++ src/test/java/com/nerdkapp/homebot/infrastructure/iot/RestRaspberryCameraIT.java
@@ -7,10 +7,13 @@ import static org.junit.Assert.*;
 
 public class RestRaspberryCameraIT
 {
+
+  private String endpoint = "http://192.168.1.101:8080";
+
   @Test
   public void retrieve_image() throws Exception
   {
-    RestRaspberryCamera camera = new RestRaspberryCamera();
+    RestRaspberryCamera camera = new RestRaspberryCamera(endpoint);
 
     ByteArrayResource file = camera.getPicture();
 
